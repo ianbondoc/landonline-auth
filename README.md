6409be97139c0a3f6e66171d5635ee37716bf2e7
Ian Bondoc <ibondoc@linz.govt.nz>
2022-04-28 19:48:34 +1200

    add another resource server client

---
+++ README.md
@@ -0,0 +1,22 @@
+# Keycloak Integration
+
+## Concerns
+
+- User Storage Provider
+- Frontend Client Integration
+- Service Integration
+- Theming
+- Frontend -> Service Security
+- Service -> Service Security
+- Roles/Permission Mapping
+- React Components
+- Spring Components
+- CORS
+- CSRF/XSRF
+- Claims customization
+- User Info Endpoint
+- Token Verification
+- Password Change
+- Temporary Password (Required Action)
+- Forgot Password
+- 
--- keycloak-image/setup_realm.sh
+++ keycloak-image/setup_realm.sh
@@ -14,7 +14,8 @@ USER_PROVIDER=linz-user-provider
 DB_URL="jdbc:informix-sqli://host.docker.internal:9088/landonline_dev:OPTOFC=1;IFX_LOCK_MODE_WAIT=5;IFX_ISOLATION_LEVEL=2U;IFX_LO_READONLY=1"
 DB_USERNAME=informix
 DB_PASSWORD=in4mix
-SERVICE_CLIENT_SECRET=pp8k7aPN2poVH1ypexeF2CDq2SpZ34WH
+SERVICE_CLIENT_SECRET_A=pp8k7aPN2poVH1ypexeF2CDq2SpZ34WH
+SERVICE_CLIENT_SECRET_B=another_secret
 
 # login
 while :; do
@@ -193,13 +194,17 @@ $KCADM create clients/$CID/protocol-mappers/models -r $REALM \
 # update frontend client
 #$KCADM update clients/$CID -r $REALM -s ...
 
-# create service client
-CID=$($KCADM create clients -r $REALM -s clientId=search-service \
-  -s bearerOnly=true -s secret=$SERVICE_CLIENT_SECRET -i)
+# create service client a
+CID=$($KCADM create clients -r $REALM -s clientId=search-service-a \
+  -s bearerOnly=true -s secret=$SERVICE_CLIENT_SECRET_A -i)
+
+# create service client b
+CID=$($KCADM create clients -r $REALM -s clientId=search-service-b \
+  -s bearerOnly=true -s secret=$SERVICE_CLIENT_SECRET_B -i)
 
 #$KCADM update clients/$CID -r $REALM -s secret=$SERVICE_CLIENT_SECRET
 
-# create user storage provider
+# use LINZ user storage provider
 $KCADM create components -r $REALM -s name=$USER_PROVIDER \
   -s providerId=$USER_PROVIDER \
   -s providerType=org.keycloak.storage.UserStorageProvider \
